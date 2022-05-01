f03243d301f4fa9f49ca3f9d01d178116ea1d45b
Tomek <paluch.t@wp.pl>
2022-05-01 14:33:15 +0200

    Initial Commit

---
+++ BinarAppRecruitmentTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BinarAppRecruitmentTask
+//
+//  Created by Tomasz Paluch on 01/05/2022.
+//  Copyright © 2022 Tomasz Paluch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
